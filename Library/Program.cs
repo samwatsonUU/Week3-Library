5b886430c9b8125c3c03d63393a458b6ca9ed3c2
samwatsonUU <Watson-S28@ulster.ac.uk>
2024-10-07 15:58:40 +0100

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Book
+{
+
+    String Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine("");
+
+    }
+    static void Main(String[] args)
+    {
+
+        Book book = new Book();
+
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "12345678";
+
+        Book book1 = new Book();
+
+        book1.Title = "OOP";
+        book1.Author = "Sam Watson";
+        book1.ISBN = "9101111213";
+
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
+    }
+
+
+}
