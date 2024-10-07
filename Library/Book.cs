867b4d284322bd0714fac8ece92eb1e021f8a865
samwatsonUU <Watson-S28@ulster.ac.uk>
2024-10-07 17:03:14 +0100

    Added properties and encapsulation

--- Library/Book.cs
+++ Library/Book.cs
@@ -4,25 +4,43 @@ namespace Library
     class Book
     {
 
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
 
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
 
         }
 
         public void DisplayInfo()
         {
 
-            Console.WriteLine($"Book title: {Title}");
-            Console.WriteLine($"Book author: {Author}");
-            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine($"Book title: {title}");
+            Console.WriteLine($"Book author: {author}");
+            Console.WriteLine($"Book ISBN: {isbn}");
             Console.WriteLine("");
 
         }
--- Library/Program.cs
+++ Library/Program.cs
@@ -10,6 +10,9 @@ class Program
         Book samsBook = new Book("Sam's Book", "Sam Watson", "77777");
         samsBook.DisplayInfo();
 
+        Book samsSecondBook = new Book("Sam's Second Book", "Sam Watson", "10101213");
+        samsSecondBook.DisplayInfo();
+
     }
 
 }
